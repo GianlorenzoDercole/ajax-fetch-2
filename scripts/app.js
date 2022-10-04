1089d53d04edd8e7c0e01a7f0eee5d055be5fa51
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-04 15:45:28 -0700

    added console log and notes

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,14 @@
+console.log('app.js')
+
 fetch('https://cat-fact.herokuapp.com/facts')
 
     .then(responseData => {
+        // does not run until we hear back from cat facts
+        //convert data payload into readable json
         return responseData.json()
     })
     .then(jsonData => {
+
         console.log(jsonData)
     })
+// console.log the data
