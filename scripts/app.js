410e20499777d273d0f5a0a6cbc23b18e0e9ce1b
GianlorenzoDercole <johndercole01@gmail.com>
2022-10-05 11:06:54 -0700

    added event listener and changed results to ten

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,54 @@
-console.log('app.js')
+// console.log('app.js')
 
-fetch('https://cat-fact.herokuapp.com/facts')
+// fetch('https://cat-fact.herokuapp.com/facts')
 
-    .then(responseData => {
-        // does not run until we hear back from cat facts
-        //convert data payload into readable json
-        return responseData.json()
-    })
-    .then(jsonData => {
+//     .then(responseData => {
+//         // does not run until we hear back from cat facts
+//         //convert data payload into readable json
+//         return responseData.json()
+//     })
+//     .then(jsonData => {
 
-        console.log(jsonData)
-    })
-// console.log the data
+//         console.log(jsonData)
+//     })
+// // console.log the data
+
+
+
+
+
+//
+
+// const url = 'http://www.redit.com/search.json?q=kittens'
+//     // fetch data
+// fetch(url)
+//     // jsonify data
+//     .then(responseData => responseData.json()) // implicit return
+//     // do something with data
+//     .then(console.log())
+//     // handle error
+//     .catch(err => {
+//         console.error('no', err)
+//     })
+
+
+
+
+//
+
+
+
+const peopleUrl = 'http://randomuser.me/api/?results=10'
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(peopleUrl)
+
+        .then(responseData => responseData.json())
+
+        .then(jsonData => {
+            console.log(jsonData)
+        })
+        .catch(err => {
+            console.error('no')
+        })
+})
